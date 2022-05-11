377defa0f9e2b6a82002eab7aaa2edb49ce4cb20
Spencer Berggreen <spencerberggreen@gmail.com>
2022-05-11 11:11:07 -0500

    added comparison notes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 // let user, password, hint;
 // user = "john";
-password = "1234";
+// password = "1234";
 // hint = "numbers ;)";
 
 // const BIRTHDAY = "01.22.1996";
@@ -20,8 +20,8 @@ password = "1234";
 // console.log(user,password,hint,BIRTHDAY);
 // const string = 'yes, hello! I\'m const "string" :)';
 // console.log(string);
-const greeting = `my favorite numbers are ${password}`;
-console.log(greeting);
+// const greeting = `my favorite numbers are ${password}`;
+// console.log(greeting);
 // const joined = `${user} ${hint}`;
 // console.log(joined)
 // let test = 10;
@@ -29,15 +29,25 @@ console.log(greeting);
 // console.log(typeof passwordToNumber);
 // document.getElementById("answer").innerHTML = txt;
 
-let letters = "abcdefg";
-let slicePos = letters.slice (1,3);
-let sliceNeg = letters.slice (-5,-3);
-let substringPos = letters.substring (1,3);
-let substringNeg = letters.substring (-6,3); // Negative values treated as 0
-let substrPos = letters.substr (1,2); // 2nd parameter in substr specifies the length of the selection
-let substrNeg = letters.substr (-6,2);
+// let letters = "abcdefg";
+// let slicePos = letters.slice (1,3);
+// let sliceNeg = letters.slice (-5,-3);
+// let substringPos = letters.substring (1,3);
+// let substringNeg = letters.substring (-6,3); // Negative values treated as 0
+// let substrPos = letters.substr (1,2); // 2nd parameter in substr specifies the length of the selection
+// let substrNeg = letters.substr (-6,2);
 
-console.log (slicePos, sliceNeg);
-console.log (substringPos, substringNeg);
-console.log (substrPos, substrNeg);
+// console.log (slicePos, sliceNeg);
+// console.log (substringPos, substringNeg);
+// console.log (substrPos, substrNeg);
 
+// let sadMsg = 'I\'m sAd and sAD';
+// let happyMsg = sadMsg.replace(/SAD/ig, 'happy');
+// let capsMsg = happyMsg.toUpperCase();
+// console.log (capsMsg);
+
+console.log (
+  0 == false,
+  0 === false,
+  0 !== false, 
+);
